0423207dc81d14f3288393f3f19f7a0e11ebb955
imaks79 <imaks9990@gmail.com>
2023-04-14 00:46:27 +0500

    Fixes (В ожидании njit)

--- crypt.py
+++ crypt.py
@@ -18,7 +18,7 @@ from bip_utils import (
 
 
 # @njit(parallel = True)
-# @jit(parallel = True)
+@jit(parallel = True)
 def start_proc(add_set:set, __words__:list, __nums__:dict, __wordlist__:list):
 	'''
 	add_set => <class 'set'>
@@ -34,12 +34,6 @@ def start_proc(add_set:set, __words__:list, __nums__:dict, __wordlist__:list):
 	ddd = ' '.join(random.sample(words, 11));
 	words = [w.lower() for w in ddd.split() if w.lower() in nums];
 	
-	print(nums)
-	print(len(nums))
-	print(len(words))
-
-	
-	'''
 	while True:
 		
 		words, nums, wordlist = __words__, __nums__, __wordlist__;
@@ -68,34 +62,34 @@ def start_proc(add_set:set, __words__:list, __nums__:dict, __wordlist__:list):
 				if isOk(cand, nums):
 					seed_bytes = Bip39SeedGenerator(result).Generate();
 					bip44_mst_ctx = Bip44.FromSeed(seed_bytes, Bip44Coins.BITCOIN);
-		            # bip49_mst_ctx = Bip49.FromSeed(seed_bytes, Bip49Coins.BITCOIN);
-		            # bip84_mst_ctx = Bip84.FromSeed(seed_bytes, Bip84Coins.BITCOIN);
-		            # bip86_mst_ctx = Bip86.FromSeed(seed_bytes, Bip86Coins.BITCOIN);
+					bip49_mst_ctx = Bip49.FromSeed(seed_bytes, Bip49Coins.BITCOIN);
+					bip84_mst_ctx = Bip84.FromSeed(seed_bytes, Bip84Coins.BITCOIN);
+					bip86_mst_ctx = Bip86.FromSeed(seed_bytes, Bip86Coins.BITCOIN);
 					bip44_acc_ctx = bip44_mst_ctx.Purpose().Coin().Account(0);
-		            # bip49_acc_ctx = bip49_mst_ctx.Purpose().Coin().Account(0);
-		            # bip84_acc_ctx = bip84_mst_ctx.Purpose().Coin().Account(0);
-		            # bip86_acc_ctx = bip86_mst_ctx.Purpose().Coin().Account(0);
+					bip49_acc_ctx = bip49_mst_ctx.Purpose().Coin().Account(0);
+					bip84_acc_ctx = bip84_mst_ctx.Purpose().Coin().Account(0);
+					bip86_acc_ctx = bip86_mst_ctx.Purpose().Coin().Account(0);
 					bip44_chg_ctx = bip44_acc_ctx.Change(Bip44Changes.CHAIN_EXT);
-		            # bip49_chg_ctx = bip49_acc_ctx.Change(Bip44Changes.CHAIN_EXT);
-		            # bip84_chg_ctx = bip84_acc_ctx.Change(Bip44Changes.CHAIN_EXT);
-		            # bip86_chg_ctx = bip86_acc_ctx.Change(Bip44Changes.CHAIN_EXT);
+					bip49_chg_ctx = bip49_acc_ctx.Change(Bip44Changes.CHAIN_EXT);
+					bip84_chg_ctx = bip84_acc_ctx.Change(Bip44Changes.CHAIN_EXT);
+					bip86_chg_ctx = bip86_acc_ctx.Change(Bip44Changes.CHAIN_EXT);
 					print(result);
 					for i in range(20):
-		                # bip32_der_ctx = bip32_mst_eth.DerivePath("m/44'/60'/0'/0/" + str(i))
+		                # TODO: bip32_der_ctx = bip32_mst_eth.DerivePath("m/44'/60'/0'/0/" + str(i))
 						bip44_addr_ctx = bip44_chg_ctx.AddressIndex(i);
 						seed44 = bip44_addr_ctx.PublicKey().ToAddress();
-		                # bip49_addr_ctx = bip49_chg_ctx.AddressIndex(i);
-		                # seed49 = bip49_addr_ctx.PublicKey().ToAddress();
-		                # bip84_addr_ctx = bip84_chg_ctx.AddressIndex(i);
-		                # seed84 = bip84_addr_ctx.PublicKey().ToAddress();
-		                # bip86_addr_ctx = bip86_chg_ctx.AddressIndex(i);
-		                # seed86 = bip86_addr_ctx.PublicKey().ToAddress();
-						# ######### ######### ######### ######### #########
-		                # eth_addr = EthAddrEncoder.EncodeKey(bip32_der_ctx.PublicKey().KeyObject())
-		                # eth = ((eth_addr) [2:]) # если ваши адреса ETH начинаются без 0x в начале
+						bip49_addr_ctx = bip49_chg_ctx.AddressIndex(i);
+						seed49 = bip49_addr_ctx.PublicKey().ToAddress();
+						bip84_addr_ctx = bip84_chg_ctx.AddressIndex(i);
+						seed84 = bip84_addr_ctx.PublicKey().ToAddress();
+						bip86_addr_ctx = bip86_chg_ctx.AddressIndex(i);
+						seed86 = bip86_addr_ctx.PublicKey().ToAddress();
+
+		                # TODO: eth_addr = EthAddrEncoder.EncodeKey(bip32_der_ctx.PublicKey().KeyObject())
+		                # TODO: eth = ((eth_addr) [2:]) # если ваши адреса ETH начинаются без 0x в начале
 
 		                # TODO: Раскоментировать стоку ниже, если ваши адреса ETH начинаются с 0x
-		                # eth = ((eth_addr)) # если ваши адреса ETH начинаются c 0x в начале
+		                # TODO: eth = ((eth_addr)) # если ваши адреса ETH начинаются c 0x в начале
 		                
 						if seed44 in add_set or seed49 in add_set or seed84 in add_set or seed86 in add_set:
 							print(result);
@@ -105,4 +99,3 @@ def start_proc(add_set:set, __words__:list, __nums__:dict, __wordlist__:list):
 							d.close();
 						elif len(words) == 12:
 							print('OK' if isOk(words, nums) else 'Invalid', 'wordlist checksum');
-'''
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from utils import getmsg
 from crypt import start_proc
 
 
-
 # @jit(parallel = True)
 def main():
 	btc_path = os.path.join('data','btc.txt'); 
@@ -20,7 +19,7 @@ def main():
 	getmsg();
 
 
-	with open(btc_path) as f:
+	with open(btc_path, 'r') as f:
 		for line in f: 
 			if line.strip(): 
 				add_set.add(line.strip());
@@ -28,13 +27,14 @@ def main():
               
 	with open(eng_path, 'r') as f:
 		words = f.read().splitlines();
+
+	with open(eng_path, 'r') as f:	
 		i = 0;
 		for word in f:
 			nums[word.strip()] = i;
 			wordlist.append(word.strip());
 			i += 1;
 
-	
 	start_proc(add_set, words, nums, wordlist);
 
 
